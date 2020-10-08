c1feaa7a4ca373757feb54b06949e5584c2750bb
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2020-10-09 01:53:55 +0400

    _

--- sales-page/main.js
+++ sales-page/main.js
@@ -82,12 +82,11 @@ function myXiconclick() {
 
 
 
-
-
-
-  function allowNumbersOnly(e) {
-    var code = (e.which) ? e.which : e.keyCode;
-    if (code > 31 && (code < 48 || code > 57)) {
-        e.preventDefault();
+  function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
     }
+    return true;
 }
