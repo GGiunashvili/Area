74f53b224640e696efed15b6e330294e9b82590b
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2020-10-09 01:40:22 +0400

    ++

--- sales-page/main.js
+++ sales-page/main.js
@@ -79,3 +79,23 @@ function saveMessage(name, company, email, phone, message){
 function myXiconclick() {
     document.getElementById("exampleModalCenter").style.display = "none";
   }
+
+
+
+  function validate(evt) {
+    var theEvent = evt || window.event;
+  
+    // Handle paste
+    if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
+    } else {
+    // Handle key press
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+    }
+    var regex = /[0-9]|\./;
+    if( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+  }
