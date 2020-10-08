fd648cc1d9d0c85090e7aa60f01cb094e042ec39
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2020-10-09 01:51:27 +0400

    ++

--- sales-page/main.js
+++ sales-page/main.js
@@ -82,20 +82,12 @@ function myXiconclick() {
 
 
 
-  function validate(evt) {
-    var theEvent = evt || window.event;
-  
-    // Handle paste
-    if (theEvent.type === 'paste') {
-        key = event.clipboardData.getData('text/plain');
-    } else {
-    // Handle key press
-        var key = theEvent.keyCode || theEvent.which;
-        key = String.fromCharCode(key);
+
+
+
+  function allowNumbersOnly(e) {
+    var code = (e.which) ? e.which : e.keyCode;
+    if (code > 31 && (code < 48 || code > 57)) {
+        e.preventDefault();
     }
-    var regex = /[0-9]|\./;
-    if( !regex.test(key) ) {
-      theEvent.returnValue = false;
-      if(theEvent.preventDefault) theEvent.preventDefault();
-    }
-  }
+}
