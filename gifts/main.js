4a1f264ee7456edbc51b10e8d7e467309a3439ab
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2020-12-21 11:51:44 +0400

    countdown

--- gifts/main.js
+++ gifts/main.js
@@ -15,6 +15,18 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+if (seconds < 10) {
+    seconds = '0' + seconds;
+}
+if (minutes < 10) {
+    minutes = '0' + minutes;
+}
+if (hours < 10) {
+    hours = '0' + hours;
+}
+if (days < 10) {
+  days = '0' + days;
+}
     
   // Output the result in an element with id="demo"
  
