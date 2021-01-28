feea805dbf0d144e4913677352f1b76a6b0f511e
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2021-01-29 00:01:36 +0400

    rotate like visa+

--- trade-in/main.js
+++ trade-in/main.js
@@ -1,4 +1,27 @@
 
-if ($('.collapse').hasClass('show')) {
-    $('.footer__col-one__title__txt').parent().css('transform', 'rotate(45deg)');
-}
+// $('.footer__col-one__title__txt').click( function() {
+//     $(this).addClass('rotate');           
+// } );
+
+$(document).ready(function(){
+    $(".footer__col-one__title__txt").click(function(){
+        $(this).toggleClass("rotate");
+    });
+});
+
+$('.icon-1').click( function() {
+    $('.icon-2').removeClass('rotate');
+    $('.icon-3').removeClass('rotate'); 
+                                               
+} );
+$('.icon-2').click( function() {
+    $('.icon-1').removeClass('rotate');
+    $('.icon-3').removeClass('rotate'); 
+                                               
+} );
+
+$('.icon-3').click( function() {
+    $('.icon-1').removeClass('rotate');
+    $('.icon-3').removeClass('rotate'); 
+                                               
+} );
