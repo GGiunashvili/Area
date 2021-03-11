d4dc92fb001150fa95be222ddf93644c4f078fad
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2021-03-11 13:48:56 +0400

    fitness 1.0

---
+++ fitness/main.js
@@ -0,0 +1,73 @@
+
+// $('.footer__col-one__title__txt').click( function() {
+//     $(this).addClass('rotate');           
+// } );
+
+$(document).ready(function(){
+    $(".icon-1").click(function(){
+        $(this).toggleClass("rotate");
+    });
+});
+$(document).ready(function(){
+    $(".icon-2").click(function(){
+        $(this).toggleClass("rotate");
+    });
+});
+$(document).ready(function(){
+    $(".icon-3").click(function(){
+        $(this).toggleClass("rotate");
+    });
+});
+
+$('.icon-1').click( function() {
+    $('.icon-2').removeClass('rotate');
+    $('.icon-3').removeClass('rotate'); 
+                                               
+} );
+$('.icon-2').click( function() {
+    $('.icon-1').removeClass('rotate');
+    $('.icon-3').removeClass('rotate'); 
+                                               
+} );
+
+$('.icon-3').click( function() {
+    $('.icon-1').removeClass('rotate');
+    $('.icon-2').removeClass('rotate'); 
+                                               
+} );
+
+
+
+
+
+
+
+
+
+///extra native footer////
+$(document).ready(function() {
+
+    $(".toggle-accordion").on("click", function() {
+      var accordionId = $(this).attr("accordion-id"),
+        numPanelOpen = $(accordionId + ' .collapse.in').length;
+      
+      $(this).toggleClass("active");
+  
+      if (numPanelOpen == 0) {
+        openAllPanels(accordionId);
+      } else {
+        closeAllPanels(accordionId);
+      }
+    })
+  
+    openAllPanels = function(aId) {
+      console.log("setAllPanelOpen");
+      $(aId + ' .panel-collapse:not(".in")').collapse('show');
+    }
+    closeAllPanels = function(aId) {
+      console.log("setAllPanelclose");
+      $(aId + ' .panel-collapse.in').collapse('hide');
+    }
+       
+  });
+  //////
